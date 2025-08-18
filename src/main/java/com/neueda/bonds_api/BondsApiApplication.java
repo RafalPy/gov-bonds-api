1bbf26f85baded17e46e89dfe23a8b03daec4b97
Rafal The Great <rafalpawezowski@gmail.com>
2025-08-18 09:34:30 +0000

    initial commit

---
+++ src/main/java/com/neueda/bonds_api/BondsApiApplication.java
@@ -0,0 +1,13 @@
+package com.neueda.bonds_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BondsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BondsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neueda/bonds_api/BondsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neueda.bonds_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BondsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
